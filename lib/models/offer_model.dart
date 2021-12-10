3fcf0b6ebce58ed4e36535bcdbfc1782a3d8fe95
brunolemes <brunolemes318@gmail.com>
2021-12-09 23:54:05 -0300

    Refeito o model responsavel pelo gerenciamento de datas e estado dos dias

--- lib/models/offer_model.dart
+++ lib/models/offer_model.dart
@@ -1,1 +1,16 @@
-class UserOffer{}
+import 'product_model.dart';
+
+class UserOffer {
+  String name, dataInicio, dataFim;
+  double desconto;
+  List<Product_Model> listdeItens;
+
+  UserOffer(
+      {required this.name,
+      required this.dataInicio,
+      required this.dataFim,
+      this.desconto = 0.0,
+      required this.listdeItens});
+
+  
+}
